53b9c3cff2c93d2b4a0d621c710ab7bfbb33fb76
AaronJin <aaron.merino@mail.utoronto.ca>
2018-12-22 05:51:34 -0500

    vector times matrix

--- matrix44.py
+++ matrix44.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import math
+import vector3
 
 class Matrix44:
 
@@ -23,4 +24,37 @@ class Matrix44:
                                         self.m[r][3] * rhsM.get_entry(3, c))
 
         return result
-                                        
+
+    """
+    v: xyz vector
+    return: xyz vector
+
+    a 1x4 point times a 4x4 matrix
+    """
+    def multVecMatrix(self, v: Vec3) -> Vec3:
+        result = Vec3():
+        result.x = v.x * self.m[0][0] + v.y * self.m[1][0] + v.z * self.m[2][0] + self.m[3][0]
+        result.y = v.x * self.m[0][1] + v.y * self.m[1][1] + v.z * self.m[2][1] + self.m[3][1]
+        result.z = v.x * self.m[0][2] + v.y * self.m[1][2] + v.z * self.m[2][2] + self.m[3][2]
+        w = v.x * self.m[0][3] + v.y * self.m[1][3] + v.z * self.m[2][3] + self.m[3][3]
+
+        if (w != 1 && w != 0):
+            result.x = result.x / w
+            result.y = result.y / w
+            result.z = result.z / w
+
+        return result
+    
+    """
+    v: xyz vector
+    return: xyz vector
+
+    a 1x4 vector times a 4x4 matrix (note: translation does not matter for vectors)
+    """
+    def multDirMatrix(self, v: Vec3) -> Vec3:
+        result = Vec3():
+        result.x = v.x * self.m[0][0] + v.y * self.m[1][0] + v.z * self.m[2][0]
+        result.y = v.x * self.m[0][1] + v.y * self.m[1][1] + v.z * self.m[2][1]
+        result.z = v.x * self.m[0][2] + v.y * self.m[1][2] + v.z * self.m[2][2]
+
+        return result
